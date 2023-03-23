2868086624d88db9612c463f9ae7617a65739091
Ashutosh3991 <ashutosh.tripathi@zemosolabs.com>
2023-03-23 09:43:11 +0530

    Completed tilll parentand child assignmnets

---
+++ sign-in-page/src/components/atoms/button/Button.tsx
@@ -0,0 +1,25 @@
+
+import "./Button.css"
+export interface ButtonProps {
+  label: string
+  type : 'button'
+  color : string
+  url? : any
+}
+
+const Button = ({ label, color, url }:ButtonProps) => {
+  const buttonClass = `button ${color}`;
+  return (
+    <button className={buttonClass} >
+      <div className='buttonStyle'>
+        {url}
+        <p>{label}</p>
+      </div>
+      
+    </button>
+  )
+}
+
+export default Button;
+
+
---
+++ sign-in-page/src/components/molecules/input/Input.tsx
@@ -0,0 +1,26 @@
+
+import "./Input.css"
+export interface InputProps {
+
+  type : 'input field'
+  cls : string
+  text : string
+  level : string
+  typeOfField : string
+}
+
+const Input = ({ cls, text, level, typeOfField }:InputProps) => {
+  const inputClass = `input ${cls}`;
+  const Text = text;
+  const txt = level;
+  const typeText = typeOfField;
+  return (
+    <label>{txt}
+      <input type={typeText} className={inputClass} placeholder={Text} />
+    </label>
+  )
+}
+
+export default Input;
+
+
